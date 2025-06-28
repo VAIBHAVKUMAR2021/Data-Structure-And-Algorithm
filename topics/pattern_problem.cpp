0180d23fe930c3552f9b3a360f1f4c5a70c58b4b
vaibhavkumar <kumarvaibhav8735@gmail.com>
2025-06-28 23:10:09 +0530

    pyramid pattern

--- topics/pattern_problem.cpp
+++ topics/pattern_problem.cpp
@@ -2,13 +2,90 @@
 using namespace std;
 
 int main(){
-    //inverted half pyramid
-    for (int row=0;row<5;row=row+1){
-        for(int col=0;col<5-row;col=col+1){
-            cout << "* ";
+    int n= 6;
+    for (int row=0;row<n;row=row+1){
+        for(col=0;col=n-row-1;col=col+1){
+              cout<<" ";
         }
-        cout << endl;
+        for(col=0; col=row+1;row=row+1){
+          if(col==0 || col=row+1-1){
+            cout<<"* ";
+          }else{
+            cout<<" ";
+          }
+        }
+        cout<<endl;
     }
+    //diamond pattern
+    // int num = 8;
+    // int n=num/2;
+    // for (int  row=0; row<n; row=row+1)
+    // {
+    //     for(int col=0;col<n-row-1;col=col+1){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0;col<row+1;col=col+1){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    //  for (int  row=0; row<n; row=row+1)
+    // {
+    //     for(int col=0;col<row;col=col+1){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0;col<n-row;col=col+1){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //inverted full pyramid
+    //  for (int  row=0; row<5; row=row+1)
+    // {
+    //     for(int col=0;col<5+row-5;col=col+1){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0;col<5-row;col=col+1){
+    //         cout<<" *";
+    //     }
+    //     cout<<endl;
+    // }
+    // fill pyramid
+    // for (int  row=0; row<5; row=row+1)
+    // {
+    //     for(int col=0;col<5-row-1;col=col+1){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0;col<row+1;col=col+1){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //inverted numeric half pyramid
+    //  for(int row =0;row<5;row=row+1){
+    //     for(int col=0;col<5-row;col=col+1){
+    //         cout<<col+1<<" " ;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // numeric half pyramid
+    // for(int row =0;row<5;row=row+1){
+    //     for(int col=0;col<row+1;col=col+1){
+    //         cout<<col+1<<" " ;
+    //     }
+    //     cout<<endl;
+    // }
+
+    //inverted half pyramid
+    // for (int row=0;row<5;row=row+1){
+    //     for(int col=0;col<5-row;col=col+1){
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
     // half pyramid
     // for (int row = 0; row < 4; row=row+1){
     //     for(int col = 0; col<row+1;col=col+1){
@@ -45,7 +122,6 @@ int main(){
 
 // int ROWS = 8;
 // int COLS =6;
-
 // for(int row = 0; row < ROWS; row=row+1)
 // {
 //     for (int col = 0; col < COLS; col= col+1)
@@ -64,7 +140,6 @@ int main(){
 //     }
 //     cout<<endl;    
 // }
-
 
 return 0;
 }
